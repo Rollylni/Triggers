5e75063d65cfbebd6b9af9c6f0671b473240cb61
Mark A <62455812+Rollylni@users.noreply.github.com>
2020-12-08 02:11:10 +0300

    Add files via upload

---
+++ Triggers.php
@@ -0,0 +1,46 @@
+<?php
+
+class Triggers {
+  
+    /**
+     * 
+     * @var \Closure[][]
+     */
+    protected $triggers = [];
+    
+    /**
+     * 
+     * @param scalar $id
+     * @param \Closure|callable $trigger
+     */
+    public function addTrigger($id, $trigger) {
+        $this->triggers[$id][] = $trigger;
+    }
+    
+    /**
+     * 
+     * @param scalar $id
+     */
+    public function removeTriggers($id) {
+        unset($this->triggers[$id]);
+    }
+    
+    /**
+     * 
+     * @param scalar $id
+     * @param mixed[] $args
+     * @return bool
+     */
+    public function handleTriggers($id, array $args = []) {
+        if (!isset($this->triggers[$id])) {
+            return false;
+        }
+        
+        foreach ($this->triggers[$id] as $trigger) {
+            if (($trigger instanceof \Closure) && \is_callable($trigger)) {
+                \call_user_func_array($trigger, $args);
+            }
+        }
+        return true;
+    }
+}
